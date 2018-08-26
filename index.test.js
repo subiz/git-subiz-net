1f0411f16e3443c18a5bbd093a21f47c36fc7112
Phạm Kiều Thanh <hhnnaahhtt@gmail.com>
2018-08-26 12:18:01 +0700

    test go-get handler

--- index.test.js
+++ index.test.js
@@ -10,3 +10,9 @@ test('get repo url', () => {
 	expect(index.getRepoUrl("account")).toBe("https://bitbucket.org/subiz/account.git")
 	expect(index.getRepoUrl("idgen")).toBe("https://source.developers.google.com/p/subiz-version-4/r/idgen")
 })
+
+test('handle go-get', () => {
+	expect(index.handleGoGet("account")).toMatch(/<meta name=\"go-import\" content=\"git\.subiz\.net\/account git https:\/\/bitbucket.org\/subiz\/account\.git\">/)
+	expect(index.handleGoGet("idgen")).toMatch(/<meta name=\"go-import\" content=\"git\.subiz\.net\/idgen git https:\/\/source\.developers\.google\.com\/p\/subiz-version-4\/r\/idgen\">/)
+	expect(index.handleGoGet("payment")).toMatch(/<meta name=\"go-import\" content=\"git\.subiz\.net\/payment git https:\/\/gitlab.com\/subiz\/payment\.git\">/)
+})
