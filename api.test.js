d053119fad0f997521ea5982eea3e5bf5f497e8c
Phạm Kiều Thanh <hhnnaahhtt@gmail.com>
2018-09-04 11:10:07 +0700

    add get github and gitlab commit from branch

---
+++ api.test.js
@@ -0,0 +1,16 @@
+var index = require("./index.js")
+
+test('get github commit', async () => {
+	let [commit, err] = await index.getGithubCommit("octocat/Hello-World", "master")
+	expect(err).toBe(undefined)
+	expect(commit).toBe('7fd1a60b01f91b314f59955a4e4d4e80d8edf11d')
+
+});
+
+
+test('get gitlab commit', async () => {
+	let token = process.env.GITLAB_TOKEN
+	let [commit, err] = await index.getGitlabCommit(token, "thanhpk/testcommit", "master")
+	expect(err).toBe(undefined)
+	expect(commit).toBe('7297e8eb5ffdd8bdbfa578206d0e01435fac4183')
+});
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-var url = require('url');
+var url = require('url')
+var request = require('request-promise');
 
+let g_gitlabtoken = process.env.GITLAB_TOKEN
 let github = name => `https://github.com/subiz/${name}.git`
 let bbk = name => `https://bitbucket.org/subiz/${name}.git`
 
@@ -28,6 +30,8 @@ module.exports = {
 	git,
 	handleGoGet,
 	handleGit,
+	getGithubCommit,
+	getGitlabCommit,
 }
 
 function removePrefixSlash(str) {
@@ -54,17 +58,14 @@ function handleGoGet(name) {
 	name = removePrefixSlash(name)
 	let firstname = name.split("/")[0]
 	let path = getRepoUrl(firstname)
-	return `<!DOCTYPE html>
-<html>
+	return `<!DOCTYPE html><html>
 <head>
 <title>git.subiz.net (0.1.6) ${firstname} ${name} ${path}</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 <meta name="go-import" content="git.subiz.net/${firstname} git ${path}">
 <meta http-equiv="refresh" content="0"; url="${path}"/>
 </head>
-<body>
-Nothing to see here; <a href="${path}">move along</a>.
-</body>
+<body>Nothing to see here; <a href="${path}">move along</a>.</body>
 </html>`
 }
 
@@ -79,14 +80,11 @@ function git(req, res) {
 	let path = url_parts.pathname
 
 	if (!path) {
-		res.writeHead(301, {
-			'Location': 'https://gitlab.com/subiz'
-		});
+		res.writeHead(301, {'Location': 'https://gitlab.com/subiz'});
 		res.end()
 		return;
 	}
 	// for go get
-
 	if (req.method == "GET" && query['go-get'] == '1') {
 		let html = handleGoGet(path)
 		res.status(200).send(html)
@@ -97,9 +95,67 @@ function git(req, res) {
 		})
 		res.end()
 		return
+	} else if (path.endsWith("/branches/master")) {
+		let [commit, err] = getcommitFromMaster(path, g_gitlabtoken)
+		if (err) {
+			res.status(400).send(err)
+			return
+		}
+
+		res.status(200).send(commit)
+		return
 	}
 	res.writeHead(301, {
 		'Location': `https://gitlab.com/subiz`
 	})
 	res.end()
 };
+
+async function getCommitFromMaster(name, gitlabtoken) {
+	name = removePrefixSlash(name)
+	let firstname = name.split("/")[0]
+	let path = getRepoUrl(firstname)
+	console.log("GOTNAME", name, "FIST", firstname, "PATH", path)
+
+	if (path.startsWith("https://github.com")) {
+		return getGithubCommit(name, "master")
+	} else if (path.startsWith("https://gitlab.com")) {
+		return getGitlabCommit(gitlabtoken, name, "master")
+	} else if (path.startsWith("https://bitbucket")) {
+		return ["", "doesn't support bitbucket"]
+	}
+	return ["", "unknown 3rd api"]
+}
+
+async function getGithubCommit(repo, branch) {
+	try {
+		let out = await request.get({
+			url: `https://api.github.com/repos/${repo}/branches/${branch}`,
+			headers: {'User-Agent': "subiz-request"},
+
+		})
+		out = JSON.parse(out)
+		return [out.commit.sha, undefined]
+	} catch(e) {
+		return [undefined, e]
+	}
+}
+
+async function getGitlabCommit(token, repo, branch) {
+	try {
+		repo = encodeURIComponent(repo)
+		console.log(repo)
+		let out = await request.get({
+			url: `https://gitlab.com/api/v4/projects/${repo}/repository/branches/${branch}`,
+			headers: {
+				'User-Agent': 'subiz-request',
+				'Private-Token': token,
+			},
+
+		})//.auth('username', 'password', false)
+		out = JSON.parse(out)
+		return [out.commit.id, undefined]
+	} catch(e) {
+		return [undefined, e]
+	}
+}
